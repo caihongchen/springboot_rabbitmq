358b45341f992eaff2a30c3dc0cb3c1161fa0bc6
jiajia <jia18330611248@163.com>
2021-08-25 19:53:32 +0800

    贾 1.0

--- src/test/java/cn/baizhi/test/Rabbitmq.java
+++ src/test/java/cn/baizhi/test/Rabbitmq.java
@@ -5,9 +5,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class Rabbitmq {
+    public static void main(String[] args) {
+        System.out.println("1111");
+    }
     @Test
     public void te(){
         int i;
     }
+
 
 }
