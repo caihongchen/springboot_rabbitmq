0e99be211a13144d8535ff19ddb549f523c06ee2
2101class <2171489535@qq.com>
2021-08-25 20:59:55 +0800

    蔡    2.0

--- src/main/java/cn/baizhi/SpringbootRabbitmqApplication.java
+++ src/main/java/cn/baizhi/SpringbootRabbitmqApplication.java
@@ -2,7 +2,10 @@ package cn.baizhi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
+@ComponentScan("cn.baizhi.cai")
 @SpringBootApplication
 public class SpringbootRabbitmqApplication {
 
